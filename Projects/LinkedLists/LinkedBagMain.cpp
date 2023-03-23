1dc34f5243c39ffda305d0e01e6a3f0f2c8518f9
dereckduran <dereckduranm@gmail.com>
2023-03-23 08:35:03 -0400

    Adding LinkedLists templates from class

---
+++ Projects/LinkedLists/LinkedBagMain.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+#include "LinkedBag.h"
+ 
+int main(){
+      LinkedBag<int> list;
+ 
+      list.add(2);
+      list.add(4);
+      list.add(8);
+      list.add(3);
+      list.display();
+      list.addToTail(5);
+      list.removeFromTail(5);
+      
+      int x = 3;
+      int frequency = list.getFrequencyOf(x);
+      cout << "El elemento "<<x<<" esta " << frequency << "dentro de las lista" << endl;
+      LinkedBag<int>  list2(list); //Copy Constructor
+      cout << "Lista original\n";
+      list.display();
+      cout << "Lista copeada\n";
+      list2.display();
+ 
+      if (list.contains(x)){
+           cout << "El elemento "<<x<<" esta dentro de la lista\n";
+      }//end if
+      else {
+           cout << "El elemento " << x << " no se encuentra  dentro de la lista\n";
+      }//end else
+      system("pause");
+      return 0;
+}//end main
